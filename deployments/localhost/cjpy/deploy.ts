b980ee3c9649ad666263961fe629bcea8c1e7ea1
0xHaku <32897506+0xHaku@users.noreply.github.com>
2023-10-13 21:52:10 +0900

    fix deployment params

--- deployments/localhost/cjpy/deploy.ts
+++ deployments/localhost/cjpy/deploy.ts
@@ -1,5 +1,5 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
-import { DeploySpec, exp, wait, makeToken, makePriceFeed, deployCometSimple } from '../../../src/deploy';
+import { DeploySpec, exp, wait, makeToken, makePriceFeed, deployCometSimple, getConfiguration } from '../../../src/deploy';
 
 // TODO: Support configurable assets as well?
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
@@ -9,47 +9,49 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const fauceteer = await deploymentManager.deploy('fauceteer', 'test/Fauceteer.sol', []);
 
   const CJPY = await makeToken(deploymentManager, 10000000, 'CJPY', 18, 'CJPY');
-  const GOLD = await makeToken(deploymentManager, 20000000, 'GOLD', 8, 'GOLD');
-  const SILVER = await makeToken(deploymentManager, 30000000, 'SILVER', 10, 'SILVER');
+  const TXJP = await makeToken(deploymentManager, 10000000, 'TXJP', 8, 'TXJP');
+  const wstETH = await makeToken(deploymentManager, 10000000, 'wstETH', 18, 'wstETH');
+  const REWARD = await makeToken(deploymentManager, 10000000, 'REWARD', 18, 'REWARD');
 
-  const cjpyPriceFeed = await makePriceFeed(deploymentManager, 'CJPY:priceFeed', 1, 8);
-  const goldPriceFeed = await makePriceFeed(deploymentManager, 'GOLD:priceFeed', 0.5, 8);
-  const silverPriceFeed = await makePriceFeed(deploymentManager, 'SILVER:priceFeed', 0.05, 8);
+  const cjpyPriceFeed = await makePriceFeed(deploymentManager, 'CJPY:priceFeed', 0.01, 8);
+  const txjpPriceFeed = await makePriceFeed(deploymentManager, 'TXJP:priceFeed', 80, 8);
+  const wstPriceFeed = await makePriceFeed(deploymentManager, 'wstETH:priceFeed', 2000, 8);
 
   const assetConfig0 = {
-    asset: GOLD.address,
-    priceFeed: goldPriceFeed.address,
+    asset: TXJP.address,
+    priceFeed: txjpPriceFeed.address,
     decimals: (8).toString(),
-    borrowCollateralFactor: (0.9e18).toString(),
-    liquidateCollateralFactor: (0.91e18).toString(),
-    liquidationFactor: (0.95e18).toString(),
-    supplyCap: (1000000e8).toString(),
+    borrowCollateralFactor: (0.8e18).toString(),
+    liquidateCollateralFactor: (0.9e18).toString(),
+    liquidationFactor: (0.9e18).toString(),
+    supplyCap: (100000e8).toString(),
   };
 
   const assetConfig1 = {
-    asset: SILVER.address,
-    priceFeed: silverPriceFeed.address,
-    decimals: (10).toString(),
-    borrowCollateralFactor: (0.4e18).toString(),
-    liquidateCollateralFactor: (0.5e18).toString(),
-    liquidationFactor: (0.9e18).toString(),
-    supplyCap: (500000e10).toString(),
+    asset: wstETH.address,
+    priceFeed: wstPriceFeed.address,
+    decimals: (18).toString(),
+    borrowCollateralFactor: (0.85e18).toString(),
+    liquidateCollateralFactor: (0.95e18).toString(),
+    liquidationFactor: (0.95e18).toString(),
+    supplyCap: exp(50000, 18).toString(),
   };
 
   // Deploy all Comet-related contracts
   const deployed = await deployCometSimple(deploymentManager, deploySpec, {
+    baseToken: CJPY.address,
     baseTokenPriceFeed: cjpyPriceFeed.address,
     assetConfigs: [assetConfig0, assetConfig1],
   });
   const { rewards } = deployed;
 
   await deploymentManager.idempotent(
-    async () => (await GOLD.balanceOf(rewards.address)).eq(0),
+    async () => (await REWARD.balanceOf(rewards.address)).eq(0),
     async () => {
-      trace(`Sending some GOLD to CometRewards`);
-      const amount = exp(2_000_000, 8);
-      trace(await wait(GOLD.connect(signer).transfer(rewards.address, amount)));
-      trace(`GOLD.balanceOf(${rewards.address}): ${await GOLD.balanceOf(rewards.address)}`);
+      trace(`Sending some REWARD to CometRewards`);
+      const amount = exp(2_000_000, 18);
+      trace(await wait(REWARD.connect(signer).transfer(rewards.address, amount)));
+      trace(`REWARD.balanceOf(${rewards.address}): ${await REWARD.balanceOf(rewards.address)}`);
     }
   );
 
@@ -57,7 +59,7 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   trace(`Attempting to mint as ${signer.address}...`);
 
   await Promise.all(
-    [[CJPY, 1e8], [GOLD, 2e6], [SILVER, 1e7]].map(([asset, units]) => {
+    [[CJPY, 10000000], [TXJP, 10000000], [wstETH, 10000000]].map(([asset, units]) => {
       return deploymentManager.idempotent(
         async () => (await asset.balanceOf(fauceteer.address)).eq(0),
         async () => {
